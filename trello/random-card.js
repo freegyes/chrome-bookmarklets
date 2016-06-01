5959eabf48f39c5b981e00e2d88c9ad5e330f219
freegyes <freegyes@users.noreply.github.com>
2016-06-01 12:00:28 +0200

    Create random-card.js

---
+++ trello/random-card.js
@@ -0,0 +1,1 @@
+javascript:(function()%7Ba=%24(".list-card").not(".hide");b=a.eq((Math.floor(Math.random() * a.length) + 0));location.href='http://trello.com' + b.find('a').first().attr('href');%7D)()
